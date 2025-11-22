6f8cd2914a972d614fedd7dca4fdea97e933134e
Didi Purnomo <didi.purnomo24067@mhs.uingusdur.ac.id>
2025-11-22 09:05:44 +0700

    ppfix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Kita HAPUS baris "base: './'" agar Vercel bisa mengatur halaman dengan benar
-  // Jika nanti mau balik ke Android Studio, baris ini perlu ditambahkan lagi.
+  // HAPUS baris base: './' agar Vercel bisa mengatur halaman dengan benar
+  // Jika nanti mau balik ke Android Studio, baris ini perlu ditambahkan lagi manual.
 })
